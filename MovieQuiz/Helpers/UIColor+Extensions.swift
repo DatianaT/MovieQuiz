902d3a97e2f4daf3b1f16fe77fdf3605c93e1aad
Dudnik Tatiana <t.dudnik@venga.com>
2026-02-03 21:12:46 +0300

    first screen

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,6 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static let ypGreen = UIColor(named: "YP Green") ?? .green
+    static let ypRed   = UIColor(named: "YP Red") ?? .red
+}
